3877717e7c3726ff51146cdc8bd52ba544bd0c5f
Rajat <rajat.kshirsagar@coditas.com>
2023-01-06 11:53:25 +0530

    fix: added store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DumbSampleComponent } from './shared/components/dumb-sample/dumb-sample.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './core/store/reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent, DumbSampleComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot(reducers, { metaReducers })],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/feature-modules/sample/sample.module.ts
+++ src/app/feature-modules/sample/sample.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SampleRoutingModule } from './sample-routing.module';
 import { SampleSmartComponent } from './container/sample-smart/sample-smart.component';
 import { SampleAddFunctinalityComponent } from './components/sample-add-functinality/sample-add-functinality.component';
 import { SampleRemoveFunctinalityComponent } from './components/sample-remove-functinality/sample-remove-functinality.component';
-
+import { StoreModule } from '@ngrx/store';
+import { sampleFeatureKey, reducer } from './store/reducers/sample.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { SampleEffects } from './store/effects/sample.effects';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { SampleRemoveFunctinalityComponent } from './components/sample-remove-fu
   ],
   imports: [
     CommonModule,
-    SampleRoutingModule
+    SampleRoutingModule,
+    StoreModule.forFeature(sampleFeatureKey, reducer),
+    EffectsModule.forFeature([SampleEffects])
   ]
 })
 export class SampleModule { }

dc2ee13b47d78c7cfcb7ec8b3203f364201b1f41
Coditas <tanmay.saraf@coditas.com>
2023-06-08 10:33:32 +0530

    Updated the angular version from v15 to v16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,13 @@ import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DumbSampleComponent } from './shared/components/dumb-sample/dumb-sample.component';
 import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './core/store/reducers';
+//import { reducers, metaReducers } from './core/store/reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
-  declarations: [AppComponent, DumbSampleComponent],
-  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot(reducers, { metaReducers })],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
